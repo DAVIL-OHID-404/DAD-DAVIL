39b64ec81ba2dce9c853848777778dabe5d86664
DAVIL-OHID-404 <143782617+DAVIL-OHID-404@users.noreply.github.com>
2023-09-02 10:43:50 +0600

    Create Lw-BANNER.py

---
+++ Lw-BANNER.py
@@ -0,0 +1,36 @@
+#/data/data/com.termux/files/usr/etc
+clear
+echo
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        Created by 'OHId-HACKE'       |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |         TikTok: @r.b__creation       |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo
+echo -e '\033[1;96m' "YouTube: https://youtube.com/@ohid_yt
+echo -e '\033[1;92m'
+echo "Installing.................."
+echo
+apt install python -y
+pip install lolcat
+pkg install figlet
+pkg install mpv -y
+echo
+rm /data/data/com.termux/files/usr/etc/bash.bashrc
+rm /data/data/com.termux/files/usr/etc/motd
+mv bash.bashrc /data/data/com.termux/files/usr/etc
+mv lib /data/data/com.termux/files/usr/etc
+cd ..
+rm -rf BannerX
+echo
+clear
+echo
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        Created by 'OHID-HACKE'       |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |       TikTok: @'r.b__creation'       |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo
+figlet Finished | lolcat
+echo -e '\033[1;96m' "Restart your termux and see awesome look"
+ech
